41137bf92e8da575fdec4b8f430031c03c0d170d
Watcharapon <watcharapon.pck@gmail.com>
2025-10-23 08:13:13 +0000

    ขยับไอค่อนกับฟ้อนมาทางขวาอีกนิด

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -9,7 +9,7 @@ export function AppHeader() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 items-center justify-center">
+      <div className="container flex h-14 items-center justify-start pl-4">
         <Link href="/" className="flex items-center space-x-2">
           <ShoppingCart className="h-10 w-10 text-primary" />
           <h1 className="bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 bg-clip-text font-headline text-2xl md:text-3xl font-bold text-transparent">
