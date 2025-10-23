ad4d668c62a486eb3572d61c194b6a10c6cf0398
Watcharapon <watcharapon.pck@gmail.com>
2025-10-23 07:36:23 +0000

    เปลี่ยนมาใช้ // Import the functions you need from the SDKs you need imp

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyDL8o2B4XT9F0jtVrUnIGsVnTCspot12XU",
-  authDomain: "longevitybarpos.firebaseapp.com",
-  projectId: "longevitybarpos",
-  storageBucket: "longevitybarpos.firebasestorage.app",
-  messagingSenderId: "963596430153",
-  appId: "1:963596430153:web:7d405bfd5c379039540898",
-  measurementId: "G-9PSK36BCR8"
+  apiKey: "AIzaSyDNtIibzzwxUtVBKGkekLun7ZRRjstNCIQ",
+  authDomain: "genfosispos.firebaseapp.com",
+  projectId: "genfosispos",
+  storageBucket: "genfosispos.firebasestorage.app",
+  messagingSenderId: "510214243979",
+  appId: "1:510214243979:web:5139189ff699efa91b368e",
+  measurementId: "G-SV05YTRTPZ"
 };
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,16 +3,18 @@
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
+
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
+  let firebaseApp: FirebaseApp;
   if (!getApps().length) {
     // Important! initializeApp() is called without any arguments because Firebase App Hosting
     // integrates with the initializeApp() function to provide the environment variables needed to
     // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
     // without arguments.
-    let firebaseApp;
     try {
       // Attempt to initialize via Firebase App Hosting environment variables
       firebaseApp = initializeApp();
@@ -24,19 +26,22 @@ export function initializeFirebase() {
       }
       firebaseApp = initializeApp(firebaseConfig);
     }
-
-    return getSdks(firebaseApp);
+  } else {
+    firebaseApp = getApp();
   }
 
-  // If already initialized, return the SDKs with the already initialized App
-  return getSdks(getApp());
+  return getSdks(firebaseApp);
 }
 
 export function getSdks(firebaseApp: FirebaseApp) {
+  // Check if analytics is supported in the current environment
+  const analytics = typeof window !== 'undefined' ? getAnalytics(firebaseApp) : null;
+  
   return {
     firebaseApp,
     auth: getAuth(firebaseApp),
-    firestore: getFirestore(firebaseApp)
+    firestore: getFirestore(firebaseApp),
+    analytics,
   };
 }
 
