f7dc32e78b7c4f02bfcb3b1203ad3be5ecc9b3cb
Watcharapon <watcharapon.pck@gmail.com>
2025-10-23 08:15:15 +0000

    ขยายพื้นที่ซ้ายขวาในหน้า / ให้พื้นที่กว้างขึ้น

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+
 import OrderPanel from '@/components/pos/order-panel';
 
 export default function PosPage() {
   return (
-    <div className="container mx-auto p-4 md:p-6 lg:p-8 h-[calc(100vh-3.5rem)]">
+    <div className="p-4 md:p-6 lg:p-8 h-[calc(100vh-3.5rem)]">
       <OrderPanel />
     </div>
   );
