cffd730cf27c19e5f8c1d30e76320706d4b4c708
Watcharapon <watcharapon.pck@gmail.com>
2025-11-13 09:43:50 +0000

    DNAcanvas LIFE+HEALTH ใช้รูป BioAge + DNAcanvas

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -3,7 +3,7 @@ import type { Drink } from '@/lib/types';
 export const DRINKS: Drink[] = [
   { id: 'item_1', name: 'EpiSpan', price: 38000, color: 'text-gray-800', bgColor: 'bg-gray-100', imageUrl: 'https://drive.google.com/uc?export=view&id=1klP5DW3AoBqXwXFFvXxmQGGVkPgpyYmU', stock: 100, sold: 0 },
   { id: 'item_2', name: 'BioAge', price: 9000, color: 'text-gray-800', bgColor: 'bg-gray-100', imageUrl: 'https://drive.google.com/uc?export=view&id=16fNRmFzZ_9_j6xN64qoq4D72W11NHq9J', stock: 100, sold: 0 },
-  { id: 'item_3', name: 'DNAcanvas LIFE+HEALTH', price: 4500, color: 'text-gray-800', bgColor: 'bg-gray-100', imageUrl: 'https://drive.google.com/uc?export=view&id=1ciB3jt-rv9VAo6vahw6mwY9kxXiTximD', stock: 100, sold: 0 },
+  { id: 'item_3', name: 'DNAcanvas LIFE+HEALTH', price: 4500, color: 'text-gray-800', bgColor: 'bg-gray-100', imageUrl: 'https://drive.google.com/uc?export=view&id=1hsegsR8mB4Icq3iYkSr9EWgZOvCU8Ijo', stock: 100, sold: 0 },
   { id: 'item_4', name: 'BioAge + DNAcanvas', price: 12000, color: 'text-gray-800', bgColor: 'bg-gray-100', imageUrl: 'https://drive.google.com/uc?export=view&id=1hsegsR8mB4Icq3iYkSr9EWgZOvCU8Ijo', stock: 100, sold: 0 },
   { id: 'item_5', name: 'Pulsetto', price: 18900, color: 'text-gray-800', bgColor: 'bg-gray-100', imageUrl: 'https://drive.google.com/uc?export=view&id=1QB3k15k657jPfZFlSrTk5Vj0KwPpC3UY', stock: 100, sold: 0 },
   { id: 'item_6', name: 'The Buddy Patch - Productivity Set', price: 549, color: 'text-gray-800', bgColor: 'bg-gray-100', imageUrl: 'https://drive.google.com/uc?export=view&id=1r-lPmwGU8AT3VVdrtJWM8K7Nzub2Rya4', stock: 100, sold: 0 },
