71594e38f6d445bb01676da43c0a315d1025d394
Watcharapon <watcharapon.pck@gmail.com>
2025-11-13 10:08:53 +0000

    รูป (เปลี่ยนลิ้งให้เป็นแบบแสดงผลได้)
    The Buddy Patch - Relax > https://

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -3,8 +3,8 @@ import type { Drink } from '@/lib/types';
 export const DRINKS: Drink[] = [
   { id: 'item_1', name: 'EpiSpan', price: 38000, color: 'text-gray-800', bgColor: 'bg-gray-100', imageUrl: 'https://drive.google.com/uc?export=view&id=1klP5DW3AoBqXwXFFvXxmQGGVkPgpyYmU', stock: 100, sold: 0 },
   { id: 'item_2', name: 'BioAge', price: 9000, color: 'text-gray-800', bgColor: 'bg-gray-100', imageUrl: 'https://drive.google.com/uc?export=view&id=16fNRmFzZ_9_j6xN64qoq4D72W11NHq9J', stock: 100, sold: 0 },
-  { id: 'item_3', name: 'DNAcanvas LIFE+HEALTH', price: 4500, color: 'text-gray-800', bgColor: 'bg-gray-100', imageUrl: 'https://drive.google.com/uc?export=view&id=16fNRmFzZ_9_j6xN64qoq4D72W11NHq9J', stock: 100, sold: 0 },
-  { id: 'item_4', name: 'BioAge + DNAcanvas', price: 12000, color: 'text-gray-800', bgColor: 'bg-gray-100', imageUrl: 'https://drive.google.com/uc?export=view&id=16fNRmFzZ_9_j6xN64qoq4D72W11NHq9J', stock: 100, sold: 0 },
+  { id: 'item_3', name: 'DNAcanvas LIFE+HEALTH', price: 4500, color: 'text-gray-800', bgColor: 'bg-gray-100', imageUrl: 'https://drive.google.com/uc?export=view&id=1hsegsR8mB4Icq3iYkSr9EWgZOvCU8Ijo', stock: 100, sold: 0 },
+  { id: 'item_4', name: 'BioAge + DNAcanvas', price: 12000, color: 'text-gray-800', bgColor: 'bg-gray-100', imageUrl: 'https://drive.google.com/uc?export=view&id=1hsegsR8mB4Icq3iYkSr9EWgZOvCU8Ijo', stock: 100, sold: 0 },
   { id: 'item_5', name: 'Pulsetto', price: 18900, color: 'text-gray-800', bgColor: 'bg-gray-100', imageUrl: 'https://drive.google.com/uc?export=view&id=1QB3k15k657jPfZFlSrTk5Vj0KwPpC3UY', stock: 100, sold: 0 },
   { id: 'item_6', name: 'The Buddy Patch - Productivity Set', price: 549, color: 'text-gray-800', bgColor: 'bg-gray-100', imageUrl: 'https://drive.google.com/uc?export=view&id=1r-lPmwGU8AT3VVdrtJWM8K7Nzub2Rya4', stock: 100, sold: 0 },
   { id: 'item_8', name: 'The Buddy Patch - Relax', price: 219, color: 'text-gray-800', bgColor: 'bg-gray-100', imageUrl: 'https://drive.google.com/uc?export=view&id=1Vv-S9VvC7sWvZyZB7f2H9e_q2b-XvY_2', stock: 100, sold: 0 },
